fe18eafb5c3c7e84be63c0ec1b5ed5573653f47c
neutrino612 <251963120@qq.com>
2023-05-01 23:04:01 +0800

    20230501

--- 京东/js/index.js
+++ 京东/js/index.js
@@ -98,18 +98,43 @@ let elevator = document.querySelector('.elevator');
 
 
 // 实现楼层滚动的效果，文字变色效果
-let items = document.querySelectorAll('.content .items');  //获取所有的div
-let elevatorA = document.querySelectorAll('.content .a');   //获取所有的 a
+let items = document.querySelectorAll('.content .item');  //获取所有的div
+let elevatorA = document.querySelectorAll('.elevator a');   //获取所有的 a
+console.log( 'elevatorA:'+elevatorA[0]);
+console.log( 'elevatorA1:'+elevatorA[1]);
+console.log( 'elevatorA2:'+elevatorA[2]);
+console.log( 'elevatorA3:'+elevatorA[3]);
+
 
 let elevatorArr = [];//数组中存放四个数字
 
 //基础的高度
 let base =header.offsetHeight + banner.offsetHeight
+console.log( 'items.length:'+items.length);
+
+for (let i = 0;i < items.length;i++){
+    base = base + items[i].offsetHeight;
+    elevatorArr.push(base);
+
+}
+    
+function clearColor(){
+    for(let i = 0;i<elevatorA.length;i++){
+        elevatorA[i].style.color = '';
+    }
+}
 
 
 
+// 获取吸顶效果的元素
+let search = document.querySelector('.search');
+let searchM = document.querySelector('.search-m');
+let form = document.querySelector('.form');
+let searchLogo = document.querySelector('.search_logo');
 
 document.onscroll = function(){
+
+
     // 获取到滚动条垂直方向滚动了多少
     let top = document.documentElement.scrollTop  || document.body.scrollTop
     // 获取到 header的高度
@@ -117,10 +142,44 @@ document.onscroll = function(){
     let headerHeight = header.offsetHeight;   //包括  height  padding border的高度\
     // 获取到banner的高度
     let bannerHeight = banner.offsetHeight;
+
+
     // 当滚动条滚动到一定程度时，改成固定定位
     if(top >= headerHeight+bannerHeight){
         elevator.className = 'elevator elevator-fix';
+        // 让原来的搜索框变成吸顶效果的样式   样式是继承且按照search-fix的效果   所以注意写法
+        search.className='search search-fix';
+        searchM.style.height = '50px';
+        form.style.top='8px';
+        searchLogo.style.display='block';
+
     }else{
         elevator.className = 'elevator';
+        search.className='search'
+        searchM.style.height = '60px';
+        form.style.top='25px';
+        searchLogo.style.display='none';
+    }
+    if (top < header.offsetHeight + banner.offsetHeight){
+        clearColor(); 
+    }
+
+    if(top >= header.offsetHeight + banner.offsetHeight && top < elevatorArr[0]){
+        console.log( 'elevatorA:'+elevatorA[0]);
+        clearColor();
+        elevatorA[0].style.color = 'red';
+
+    }else if(top >= elevatorArr[0] && top <elevatorArr[1] ){
+        console.log( 'elevatorA:'+elevatorA[1]);
+        clearColor();        
+        elevatorA[1].style.color = 'red';
+    }else if(top >= elevatorArr[1] && top < elevatorArr[2]){
+        console.log( 'elevatorA:'+elevatorA[2]);
+        clearColor();        
+        elevatorA[2].style.color = 'red';        
+    }else if(top > elevatorArr[2]){
+        console.log( 'elevatorA:'+elevatorA[3]);
+        clearColor();        
+        elevatorA[3].style.color = 'red';        
     }
 }
